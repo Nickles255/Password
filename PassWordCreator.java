a5acc2cb02860ccb342e38691f9fe665a3b821b5
Nickles255 <Nickles255@gmail.com>
2017-09-23 08:15:28 -0700

    Add files via upload

---
+++ PassWordCreator.java
@@ -0,0 +1,36 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PassWordCreator extends Application {
+/**
+	* PassWordCreator --- create and display window for password program 
+	* This program depends on PassWordPane class to receive input, process,
+	* then return output.
+	* @author(s) Chien Lin, Lena Zheng, Qd Li
+	* @version 1.0
+	* @since 2017-09-15
+	* input: name and date
+	* output: password consisting of first two letters of name followed by a period
+	*         followed by a two digit random number followed by last for digit of date
+	* Example: 
+	*           input: name:Chris, date: 091717 
+	*           output: Ch.521717
+**/	
+	public void start(Stage primaryStage)
+    {
+        Scene scene = new Scene(new PassWordPane(), 300, 150);
+        
+        primaryStage.setTitle("Password Creator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+	
+	public static void main(String[] args) {
+		
+           launch(args);
+	}
+		
+	}
+
+}
---
+++ PassWordPane.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+
+public class PassWordPane extends GridPane {
+	public class PassWordCreator extends Application {
+		/**
+			* PassWordPane --- program to generate password based on users input 
+			* This class is dependent on PassWordCreate program to display.
+			* @author(s) Chien Lin, Lena Zheng, Qd Li
+			* @version 1.0
+			* @since 2017-09-15
+			* input: name and date
+			* output: password consisting of first two letters of name followed by a period
+			*         followed by a two digit random number followed by last for digit of date
+			* Example: 
+			*           input: name:Chris, date: 091717 
+			*           output: Ch.521717
+		**/	
+	
+	
+}
