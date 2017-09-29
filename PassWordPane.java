c08006f9c1c831ac1f377dc1c816eb46be027fac
Lena <lzheng22@mail.ccsf.edu>
2017-09-28 22:02:24 -0700

    fixed spaces in passwordpant.java

--- PassWordPane.java
+++ PassWordPane.java
@@ -9,85 +9,84 @@ import javafx.scene.text.Font;
 import java.util.Random;
 
 public class PassWordPane extends GridPane { 
-        /**
-			* PassWordPane --- program to generate password based on users input 
-			* This class is dependent on PassWordCreate program to display.
-			* @author(s) Chien Lin, Lena Zheng, Qd Li
-			* @version 1.0
-			* @since 2017-09-15
-			* input: name and date
-			* output: password consisting of first two letters of name followed by a period
-			*         followed by a two digit random number followed by last for digit of date
-			* Example: 
-			*           input: name:Chris, date: 091717 
-			*           output: Ch.521717
-		**/	
-	private Label result;
-	private TextField inputName;
-	private TextField inputDate;
+    /**
+    * PassWordPane --- program to generate password based on users input 
+    * This class is dependent on PassWordCreate program to display.
+    * @author(s) Chien Lin, Lena Zheng, Qd Li
+    * @version 1.0
+    * @since 2017-09-15
+    * input: name and date
+    * output: password consisting of first two letters of name followed by a period
+    *         followed by a two digit random number followed by last for digit of date
+    * Example: 
+    *           input: name:Chris, date: 091717 
+    *           output: Ch.521717
+    **/	
+    private Label result;
+    private TextField inputName;
+    private TextField inputDate;
 	
-		public PassWordPane() {
-	    Font font = new Font(18);
+    public PassWordPane() {
+        Font font = new Font(18);
 		 
-	     //Team Label creation.
-	     Label teamLabel = new Label("Chien Lin, Lena Zheng, Qd Li");
-	     teamLabel.setFont(font);
-	     GridPane.setHalignment(teamLabel, HPos.LEFT);
+        //Team Label creation.
+        Label teamLabel = new Label("Chien Lin, Lena Zheng, Qd Li");
+        teamLabel.setFont(font);
+        GridPane.setHalignment(teamLabel, HPos.LEFT);
 			
-	     //Accept Name input.
-	     Label inputNameLabel = new Label("Please enter name: ");
-	     inputNameLabel.setFont(font);
-	     GridPane.setHalignment(inputNameLabel, HPos.RIGHT);
+        //Accept Name input.
+        Label inputNameLabel = new Label("Please enter name: ");
+        inputNameLabel.setFont(font);
+        GridPane.setHalignment(inputNameLabel, HPos.RIGHT);
 			
-	     inputName = new TextField();
-	     inputName.setFont(font);
-	     inputName.setPrefWidth(200);
-	     inputName.setAlignment(Pos.CENTER);
-	     inputName.setOnAction(this::processResult);
+        inputName = new TextField();
+        inputName.setFont(font);
+        inputName.setPrefWidth(200);
+        inputName.setAlignment(Pos.CENTER);
+        inputName.setOnAction(this::processResult);
 		 
-	     //Accept Date input			
-	     Label inputDateLabel = new Label("Please enter a date: ");
-             inputDateLabel.setFont(font);
-	     GridPane.setHalignment(inputDateLabel, HPos.RIGHT);
+        //Accept Date input			
+        Label inputDateLabel = new Label("Please enter a date: ");
+        inputDateLabel.setFont(font);
+        GridPane.setHalignment(inputDateLabel, HPos.RIGHT);
 			
-	     inputDate = new TextField();
-	     inputDate.setFont(font);
-	     inputDate.setPrefWidth(200);
-	     inputDate.setAlignment(Pos.CENTER);
-	     inputDate.setPromptText("mmddyy");
-	     inputDate.setOnAction(this::processResult);
+        inputDate = new TextField();
+        inputDate.setFont(font);
+        inputDate.setPrefWidth(200);
+        inputDate.setAlignment(Pos.CENTER);
+        inputDate.setPromptText("mmddyy");
+        inputDate.setOnAction(this::processResult);
 			
-	      //Label to store Result 
-	      result = new Label();
-	      result.setFont(font);
-	      GridPane.setHalignment(result, HPos.CENTER);
+        //Label to store Result 
+        result = new Label();
+        result.setFont(font);
+        GridPane.setHalignment(result, HPos.CENTER);
 
-	      setStyle("-fx-background-color: yellow");
+        setStyle("-fx-background-color: yellow");
 
-           //set the positioning
-	       add(teamLabel, 0, 0);
-	       add(inputNameLabel,0,1);
-	       add(inputName,1,1);
-	       add(inputDateLabel,0,2);
-	       add(inputDate,1,2);
-	       add(result,0,3);
-
-        }
+        //set the positioning
+        add(teamLabel, 0, 0);
+        add(inputNameLabel,0,1);
+        add(inputName,1,1);
+        add(inputDateLabel,0,2);
+        add(inputDate,1,2);
+        add(result,0,3);
+    }
 	
-	public void processResult(ActionEvent event){
-         Random ran = new Random();// random number generator
+    public void processResult(ActionEvent event){
+        Random ran = new Random();// random number generator
        
-         String log = inputName.getText();
-       int pass =Integer.parseInt( inputDate.getText());
+        String log = inputName.getText();
+        int pass =Integer.parseInt( inputDate.getText());
        
-       //Gets the last 4 digit 
-       int fourdigit = pass%10000; 
+        //Gets the last 4 digit 
+        int fourdigit = pass%10000; 
        
-       int num = ran.nextInt(90)+10;//random number 10-99
+        int num = ran.nextInt(90)+10;//random number 10-99
        
-       /*displays the first two letter, 1-2 digit random number, 
-       and last 4 digit of the date*/
-       result.setText(log.substring(0,2) +"."+num+ fourdigit+ "");
-     }
+        /*displays the first two letter, 1-2 digit random number, 
+        and last 4 digit of the date*/
+        result.setText(log.substring(0,2) +"."+num+ fourdigit+ "");
+    }
 }	
 	
